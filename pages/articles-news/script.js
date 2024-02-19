f3e369a19237c5e8964631843f2339a74a341ddd
mav <email@example.com>
2024-02-19 18:21:48 +0300

    work with date

--- pages/articles-news/script.js
+++ pages/articles-news/script.js
@@ -1,71 +1,71 @@
 const app = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     month: [
-      "December",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "January",
+      'December',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'January',
     ],
     articles: [
       {
         id: 0,
-        img: "../../assets/news-image/1.png",
-        name: "let’s get solution for building construction work",
+        img: '../../assets/news-image/1.png',
+        name: 'let’s get solution for building construction work',
         date: 1735171200,
-        tag: "kitchan design",
+        tag: 'kitchan design',
       },
       {
         id: 1,
-        img: "../../assets/news-image/2.png",
-        name: " low cost latest invented interior designing ideas.",
+        img: '../../assets/news-image/2.png',
+        name: ' low cost latest invented interior designing ideas.',
         date: 1734825600,
-        tag: "living design",
+        tag: 'living design',
       },
       {
         id: 2,
-        img: "../../assets/news-image/3.png",
-        name: "best for any office & business interior solution",
+        img: '../../assets/news-image/3.png',
+        name: 'best for any office & business interior solution',
         date: 1735171200,
-        tag: "interior design",
+        tag: 'interior design',
       },
       {
         id: 3,
-        img: "../../assets/news-image/4.png",
-        name: "let’s get solution for building construction work",
+        img: '../../assets/news-image/4.png',
+        name: 'let’s get solution for building construction work',
         date: 1735084800,
-        tag: "kitchan design",
+        tag: 'kitchan design',
       },
       {
         id: 4,
-        img: "../../assets/news-image/5.png",
-        name: " low cost latest invented interior designing ideas.",
+        img: '../../assets/news-image/5.png',
+        name: ' low cost latest invented interior designing ideas.',
         date: 1734825600,
-        tag: "living design",
+        tag: 'living design',
       },
       {
         id: 5,
-        img: "../../assets/news-image/6.png",
-        name: "best for any office & business interior solution",
+        img: '../../assets/news-image/6.png',
+        name: 'best for any office & business interior solution',
         date: 1735084800,
-        tag: "interior design",
+        tag: 'interior design',
       },
     ],
   },
   methods: {
     getDateToString(timestamp) {
-                const date = new Date(timestamp * 1000);
-                return `${date.getDate()} ${this.month[date.getMonth()]} ${date.getFullYear()}`;
-    } 
+      const date = new Date(timestamp * 1000);
+      return `${date.getDate()} ${
+        this.month[date.getMonth()]
+      } ${date.getFullYear()}`;
     },
   },
 });
-console.log(app);
